72bf696e858e09c41ed9e2199bf8d9423594bc3e
zkharit <zkharit@gmail.com>
2022-08-24 10:33:46 -0400

    Comment out logging (for now) and implement foundation create endpoint

--- src/postgres.js
+++ src/postgres.js
@@ -1,71 +1,79 @@
 const { Client } = require('pg')
-require('dotenv').config({path: '../.env'})
+require('dotenv').config({path: '.env'})
 
-function insertIntoFoundation(email) {
-    const client = new Client()
-    client.connect()
+class Postgres {
+    constructor () {}
 
-    const query = 'INSERT INTO foundation(email) VALUES($1)'
-    const values = [email]
-
-    client.query(query, values, (err, res) => {
-
-        if(err) {
-            console.log(err.message)
-        } else {
-            console.log(res)
-        }
-        client.end()
-    })
+    insertIntoFoundation(email) {
+        const client = new Client()
+        client.connect()
+    
+        const query = 'INSERT INTO foundation(email) VALUES($1)'
+        const values = [email]
+    
+        client.query(query, values, (err, res) => {
+    
+            if(err) {
+                console.log(err.message)
+            } else {
+                // console.log(res.rows)
+                // will add this to specific logging levels later
+            }
+            client.end()
+        })
+    }
+    
+    insertIntoNonProfit(email, name, address) {
+        const client = new Client()
+        client.connect()
+    
+        const query = 'INSERT INTO nonprofit(email, name, address) VALUES($1, $2, $3)'
+        const values = [email, name, address]
+    
+        client.query(query, values, (err, res) => {
+            if(err) {
+                console.log(err.message)
+            } else {
+                // console.log(res.rows)
+                // will add this to specific logging levels later
+            }
+            client.end()
+        })
+    }
+    
+    getAllFromNonProfit() {
+        const client = new Client()
+        client.connect()
+    
+        const query = 'SELECT * from nonprofit'
+    
+        client.query(query, (err, res) => {
+            if(err) {
+                console.log(err.message)
+            } else {
+                // console.log(res.rows)
+                // will add this to specific logging levels later
+            }
+            client.end()
+        })
+    }
+    
+    getEmailFromNonProfit() {
+        const client = new Client()
+        client.connect()
+    
+        const query = 'SELECT email from nonprofit'
+    
+        client.query(query, (err, res) => {
+            if(err) {
+                console.log(err.message)
+            } else {
+                // console.log(res.rows)
+                // will add this to specific logging levels later
+            }
+            client.end()
+        })
+    }
 }
 
-function insertIntoNonProfit(email, name, address) {
-    const client = new Client()
-    client.connect()
-
-    const query = 'INSERT INTO nonprofit(email, name, address) VALUES($1, $2, $3)'
-    const values = [email, name, address]
-
-    client.query(query, values, (err, res) => {
-        if(err) {
-            console.log(err.message)
-        } else {
-            console.log(res)
-        }
-        client.end()
-    })
-}
-
-function getAllFromNonProfit() {
-    const client = new Client()
-    client.connect()
-
-    const query = 'SELECT * from nonprofit'
-
-    client.query(query, (err, res) => {
-        if(err) {
-            console.log(err.message)
-        } else {
-            console.log(res.rows)
-        }
-        client.end()
-    })
-}
-
-function getEmailFromNonProfit() {
-    const client = new Client()
-    client.connect()
-
-    const query = 'SELECT email from nonprofit'
-
-    client.query(query, (err, res) => {
-        if(err) {
-            console.log(err.message)
-        } else {
-            console.log(res.rows)
-        }
-        client.end()
-    })
-}
-
-// export {insertIntoFoundation}
+module.exports = Postgres
