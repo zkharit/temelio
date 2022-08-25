824a34821e805d8b01010b969d23651cf323c83b
zkharit <zkharit@gmail.com>
2022-08-25 10:34:56 -0400

    Add OpenAPI Initialization

--- src/server.js
+++ src/server.js
@@ -4,6 +4,8 @@ const app = express()
 const foundationRouter = require('../routes/foundation')
 const nonProfitRouter = require('../routes/nonprofit')
 const logger = require('../src/logger')
+var { initialize } = require("express-openapi");
+var swaggerUi = require("swagger-ui-express");
 
 app.set('view engine', 'ejs')
 app.use(endpointLogger)
@@ -12,6 +14,24 @@ app.use(express.json())
 
 app.use('/foundation', foundationRouter)
 app.use('/nonprofit', nonProfitRouter)
+
+// Initialize OpenAPI
+initialize({
+    app,
+    apiDoc: require("../api-v0.1.0/api-doc"),
+    paths: "./api/paths",
+  });
+
+// OPenAPI UI
+app.use(
+    "/api-documentation",
+    swaggerUi.serve,
+    swaggerUi.setup(null, {
+      swaggerOptions: {
+        url: "http://localhost:3000/api-documentation",
+      },
+    })
+  );
 
 function endpointLogger(req, res, next) {
     let scope = "server.endpointLogger():"
